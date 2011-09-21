c7dbc1b72c2539dc6a9f5c794ed1ef81625b4a23
Napech <vincent.cohenaddad@ens-lyon.fr>
2011-09-21 17:48:18 +0300

    Edited scripts/app.js via GitHub

--- scripts/app.js
+++ scripts/app.js
@@ -24,7 +24,7 @@ $(function(){
     FB.Event.subscribe('auth.sessionChange', session_handle);
     FB.Event.subscribe('auth.login', session_handle);
     FB.getLoginStatus(session_handle);
-    FB.Event.subscribe("auth.logout", function() {window.location = '/logout'});  
+    FB.Event.subscribe("auth.logout", function() {});  
   };
   (function() {
     var e = document.createElement('script'); e.async = true;
