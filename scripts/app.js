0a5f5b3f713e4e29df4a1780e348201c54b52262
Cohen-Addad Vincent <vcohenad@slsu2-10.ens-lyon.fr>
2011-09-27 15:39:21 +0200

    ca marche maintenant ahah

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,35 @@
-$(function() {
-	window.fbAsyncInit = function() {
-	    FB.init({
-		    appId  : '224111417646401', 
-		    status : true, 
-		    cookie : true,
-		    xfbml  : true
-		});
-	    
-	    FB.api('/me/friends', function(response){
-		    response.data.forEach(function(friend){
-			    //$('#friends').append('<div>'+friend.getMutualFriends()+'</div>');
-			    $('#friends').append('<div>'+JSON.stringify(friend)+'</div>');
-			});
-		});
-	    
-	    FB.XFBML.parse();
-	};
-	
-	FB.Event.subscribe('auth.sessionChange', session_handle);
-	FB.Event.subscribe('auth.login', session_handle);
-	FB.getLoginStatus(session_handle);
-	FB.Event.subscribe("auth.logout", function() {window.location = "/";});  
-	(function() {
-	    var e = document.createElement('script'); e.async = true;
-	    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-	    document.getElementById('fb-root').appendChild(e);
-	}());
+$(function(){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId : '224111417646401',
+      status : true,
+      cookie : true,
+      xfbml : true
     });
+
+    var session_handle = function(response){
+      if (!response.session) return $('#login').show();
+
+      //$('#login').hide();
+      document.getElementById("boutonlogin").value = "Log out";
+     // $('#logout').show();
+
+      FB.api('/me/friends', function(response){
+        response.data.forEach(function(friend){
+          $('#friends').append('<div>'+JSON.stringify(friend)+'</div>');
+        });
+      });
+
+      FB.XFBML.parse();
+    };
+    FB.Event.subscribe('auth.sessionChange', session_handle);
+    FB.Event.subscribe('auth.login', session_handle);
+    FB.getLoginStatus(session_handle);
+    FB.Event.subscribe("auth.logout", function() {window.location = "/";});
+  };
+  (function() {
+    var e = document.createElement('script'); e.async = true;
+    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+    document.getElementById('fb-root').appendChild(e);
+  }());
+});
