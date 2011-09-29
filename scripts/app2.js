85eaa6ae0da81146ffb7f8892860ba038a41d96b
Jean-Florent Raymond <jeanflorent@no-log.org>
2011-09-29 19:14:10 +0200

    fin de app2.js : si ça marche ça devrait afficher les amis de chaque ami.

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,42 +1,71 @@
+document.write("<script type='text/javascript' src='scripts/incl.js' charset='utf-8'></script>" );
+
 
 var compAmis = 0; //compteur pour savoir commbien de presonnes gérer
 
-var amisTbl = HashTbl(); //correspondance ID => ID des amis
+var amisTbl = HashTbl(); //correspondance ID => IDs des amis
 
 function nouvelAmis(id1,id2) {
-    amisTbl.setItem(id1,amisTbl.getItem(id1).push(id2)); // ajoute id2 à id1
-    amisTbl.setItem(id2,amisTbl.getItem(id2).push(id1)); // ajoute id1 à id2
+    if(typeof(amisTbl.getItem(id1)) != 'undefinied') { // si id1 n'a pas d'amis...
+	amisTbl.setItem(id1) = HashTbl();
+    }
+    if(typeof(amisTbl.getItem(id2)) != 'undefinied') { // si id2 n'a pas d'amis...
+	amisTbl.setItem(id2) = HashTbl();
+    }
+    
+    amisTbl.getItem(id1).setItem(id2,true)
+    amisTbl.getItem(id2).setItem(id1,true)
+    //amisTbl.setItem(id1,amisTbl.getItem(id1).push(id2)); // ajoute id2 à id1
+    //amisTbl.setItem(id2,amisTbl.getItem(id2).push(id1)); // ajoute id1 à id2
 }
 
-/*
+$(function(){
+    window.fbAsyncInit = function() {
+	FB.init({
+	    appId : '224111417646401',
+	    status : true,
+	    cookie : true,
+	    xfbml : true
+	});
 
-function displayUser(user) {
-    var userName = document.getElementById('userName');
-    var greetingText = document.createTextNode('Greetings, '
-					       + user.name + '.');
-    userName.appendChild(greetingText);
-}
+	var session_handle = function(response){
+	    if (!response.session) return $('#login').show();
 
-var appID = 224111417646401;
-if (window.location.hash.length == 0) {
-    var path = 'https://www.facebook.com/dialog/oauth?';
-    var queryParams = ['client_id=' + appID,
-		       'redirect_uri=' + window.location,
-		       'response_type=token',
-		       'scope=read_friendlists,friends_about_me'];
-    var query = queryParams.join('&');
-    var url = path + query;
-    window.open(url);
-} else {
-    var accessToken = window.location.hash.substring(1);
-    var path = "https://graph.facebook.com/me?";
-    var queryParams = [accessToken, 'callback=displayUser'];
-    var query = queryParams.join('&');
-    var url = path + query;
+	    document.getElementById("boutonlogin").value = "Log out";
 
-    // use jsonp to call the graph
-    var script = document.createElement('script');
-    script.src = url;
-    document.body.appendChild(script);        
-}
-*/
+	    FB.api(
+		{
+		    method: 'fql.query',
+		    query: 'SELECT uid1, uid2 FROM friend WHERE uid1 IN
+                                (SELECT uid2 FROM friend WHERE uid1= me())
+                                AND uid2 IN (SELECT uid2 FROM friend WHERE uid1= me())'
+		},
+		function(liste){
+		    liste.forEach(function(rep){
+			nouvelAmis(rep.uid1,rep.uid2);
+		    });
+		});
+	    
+	    var noeud_tmp;
+	    for(var i in amisTbl.items) {
+		noeud_tmp = document.createElement('amis_'+parseInt(i));
+		noeud_tmp.append('<div>'+JSON.stringify(i)+'</div>')
+		for(j in amisTbl.getItem(i)) {
+		    noeud_tmp.append('<div>    |-> '+ JSON.stringify(j)+'</div>');
+		}
+		document.getElementById('fb-root').appendChild(noeud_tmp);
+	    }
+
+	    FB.XFBML.parse();
+	};
+	FB.Event.subscribe('auth.sessionChange', session_handle);
+	FB.Event.subscribe('auth.login', session_handle);
+	FB.getLoginStatus(session_handle);
+	FB.Event.subscribe("auth.logout", function() {window.location = "/";});
+    };
+    (function() {
+	var e = document.createElement('script'); e.async = true;
+	e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+	document.getElementById('fb-root').appendChild(e);
+    }());
+});
