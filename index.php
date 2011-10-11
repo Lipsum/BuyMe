027e7de0d846db183f93e8fc2cb4dbdb1af3ed0b
Jean-Florent Raymond <jeanflorent@no-log.org>
2011-10-11 14:50:50 +0200

    new index

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="https://www.facebook.com/2008/fbml">
-   <head>
-   <title>BuyMe!</title>
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
-   <script src="scripts/raphael-min.js" type="text/javascript" charset="utf-8"></script>
-   <script src="scripts/app0.js" type="text/javascript" charset="utf-8"></script>
-   <script type="text/javascript" src="scripts/prototype.js" charset="utf-8"></script>
-   <link rel="stylesheet" href="stylesheets/style.css" type="text/css" media="screen" title="no title" charset="utf-8" />
-
-
-<!--    #canvas_container {  width: 500px;  border: 1px solid #aaa;  }  -->
-
-
+<head>
+  <title>AlgoRes template</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="scripts/raphael-min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="scripts/app.js" type="text/javascript" charset="utf-8"></script>
+  <link rel="stylesheet" href="/stylesheets/style.css" type="text/css" media="screen" title="no title" charset="utf-8" />
 </head>
 <body>
-<p id="userName"></p>
-    
-    <div id="fb-root"></div> 
-    <div id="login"><fb:login-button>Connect with Facebook</fb:login-button></div>
-    <div id="friends"></div> 
-<!--    <div id="canvas_container"></div>-->
-    </body>
-    </html>
+  <div id="fb-root"></div>
+  <div id="login"><fb:login-button>Connect with Facebook</fb:login-button></div>
+  
+  <pre id="friends"></pre>
+</body>
+</html>
